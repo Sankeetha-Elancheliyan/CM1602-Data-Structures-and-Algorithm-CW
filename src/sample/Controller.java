89ca49c5e24198a584d1f2870a0e54e155d44861
Sankeetha <119834420+Sankeetha-Elancheliyan@users.noreply.github.com>
2023-08-20 12:13:03 +0530

    Add files via upload

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Controller {
+    Queue normalCustomer = new Queue();
+    Queue highPriorityCustomer = new Queue();
+    Queue innerCircleClients = new Queue();
+
+    normalCustomerEnque(){
+        normalCustomer.enqueue();
+    }
+}
